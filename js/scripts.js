1ca62cda543bdd748fbedbf24a9c557d94b2a17f
Matthew Craig <mgcraig78@gmail.com>
2020-06-08 20:07:40 -0700

    make a short program that takes form info and turns it into an array and returns the alphabetized list

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("#groceries").submit(function(event){
+    const entry1 = $("input#entry1").val();
+    const entry2 = $("input#entry2").val();
+    const entry3 = $("input#entry3").val();
+    const entry4 = $("input#entry4").val();
+    const entry5 = $("input#entry5").val();
+    const entry6 = $("input#entry6").val();
+
+    const array = [entry1, entry2, entry3, entry4, entry5, entry6];
+    array.sort();
+    
+    $("#result").append("<li>" + array + "</li");
+    
+    event.preventDefault();
+  });
+});
+
